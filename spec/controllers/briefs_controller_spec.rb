a1566b3d1570636364edd1f8feb0b64be388ffb7
mark peng <bjmark.sina.com>
2011-12-07 10:53:33 +0800

    briefs_controller

--- spec/controllers/briefs_controller_spec.rb
+++ spec/controllers/briefs_controller_spec.rb
@@ -19,21 +19,82 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe BriefsController do
+  let(:rpm_org) { RpmOrg.create(:name=>'rpm') }
+  let(:cheil_org) { rpm_org.create_cheil_org(:name=>'cheil')}
+  let(:vendor_org) {VendorOrg.create(:name=>'vendor')}
+  
+  let(:rpm_org2) { RpmOrg.create(:name=>'rpm2') }
+  let(:cheil_org2) {CheilOrg.create(:name=>'cheil2')}
+  let(:vendor_org2) {VendorOrg.create(:name=>'vendor2')}
 
-  # This should return the minimal set of attributes required to create a valid
-  # Brief. As you add validations to Brief, be sure to
-  # update the return value of this method accordingly.
-=begin
-  def valid_attributes
-    {}
-  end
-=end
+  let(:rpm_user) { rpm_org.users.create(:name=>'rpm_user',:password=>'123')}
+  let(:cheil_user) { cheil_org.users.create(:name=>'cheil_user',:password=>'123')}
+  let(:vendor_user) { vendor_org.users.create(:name=>'vendor_user',:password=>'123')}
+
+  let(:rpm2_user) { rpm_org2.users.create(:name=>'rpm2_user',:password=>'123')}
+  let(:cheil2_user) { cheil_org2.users.create(:name=>'cheil2_user',:password=>'123')}
+  let(:vendor2_user) { vendor_org2.users.create(:name=>'vendor2_user',:password=>'123')}
+
   describe "GET index" do
     describe "not login" do
       specify{
+        get 'index'
         response.should redirect_to(new_session_path)
       }
     end
+
+    def set_current_user(user)
+      session[:user_id] = user.id
+    end
+
+    describe "current user is a rpm_user" do
+      specify{
+        set_current_user(rpm_user)
+        
+        brief1 = rpm_org.briefs.create(:name=>'brief1')
+        rpm_org2.briefs.create(:name=>'brief2')
+
+        get 'index'
+        assigns(:briefs).should eq([brief1])
+      }
+    end
+
+    describe "current user is a cheil_user" do
+      specify{
+        set_current_user(cheil_user)
+
+        brief1 = rpm_org.briefs.create(:name=>'brief1')
+
+        get 'index'
+        assigns(:briefs).should eq([])
+
+        brief1.send_to_cheil!
+
+        get 'index'
+        assigns(:briefs).should eq([brief1])
+      }
+    end
+
+    describe "current user is a vendor_user" do
+      specify{
+        set_current_user(vendor_user)
+
+        brief1 = rpm_org.briefs.create(:name=>'brief1')
+        get 'index'
+        assigns(:briefs).should eq([])
+
+        brief1.send_to_cheil!
+        get 'index'
+        assigns(:briefs).should eq([])
+
+        brief1.vendor_solutions.create(:org_id=>vendor_user.org_id)
+        get 'index'
+        assigns(:briefs).should eq([brief1])
+      }
+    end
+
+  end
+
 =begin    
     it "assigns all briefs as @briefs" do
       brief = Brief.create! valid_attributes
@@ -41,7 +102,6 @@ describe BriefsController do
       assigns(:briefs).should eq([brief])
     end
 =end
-  end
 =begin
   describe "GET show" do
     it "assigns the requested brief as @brief" do
